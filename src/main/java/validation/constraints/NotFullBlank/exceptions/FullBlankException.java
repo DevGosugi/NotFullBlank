1a41d92614fb61d6a9808fe363d58caa4f3913b2
DevGosugi <goki.sugimura89@gmail.com>
2020-06-15 01:19:56 +0900

    Add exceptions

---
+++ src/main/java/validation/constraints/NotFullBlank/exceptions/FullBlankException.java
@@ -0,0 +1,7 @@
+package validation.constraints.NotFullBlank.exceptions;
+
+public class FullBlankException extends Exception {
+    public FullBlankException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/validation/constraints/NotFullBlank/exceptions/NotStringException.java
@@ -0,0 +1,7 @@
+package validation.constraints.NotFullBlank.exceptions;
+
+public class NotStringException extends Exception {
+    public NotStringException(String msg) {
+        super(msg);
+    }
+}
