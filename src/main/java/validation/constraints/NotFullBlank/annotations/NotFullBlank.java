2aa7f3db9f11558abef26c55c890ca90ad563977
DevGosugi <goki.sugimura89@gmail.com>
2020-06-23 09:20:03 +0900

    Some changes

--- src/main/java/validation/constraints/NotFullBlank/annotations/NotFullBlank.java
+++ src/main/java/validation/constraints/NotFullBlank/annotations/NotFullBlank.java
@@ -10,6 +10,7 @@ import java.lang.annotation.*;
 // https://qiita.com/y_tochukaso/items/86e10bda6bbefbdaf983
 // http://enterprisegeeks.hatenablog.com/entry/2016/02/29/072811
 // https://qiita.com/opengl-8080/items/3926fbde5469c0b330c2#%E8%87%AA%E4%BD%9C%E3%81%AE%E3%83%90%E3%83%AA%E3%83%87%E3%83%BC%E3%82%BF%E3%81%A8%E5%88%B6%E7%B4%84%E3%82%A2%E3%83%8E%E3%83%86%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B
+// https://ti-tomo-knowledge.hatenablog.com/entry/2018/06/18/094526
 
 //@Documented
 @Constraint(validatedBy = {NotFullBlankValidator.class})
